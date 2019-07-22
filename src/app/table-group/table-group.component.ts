05c90c7e1d6f53a50ea21c1ef862fbd337b5d611
Shermam Xavier Miranda <shermam.miranda@hotmail.com>
2019-07-22 00:19:58 -0300

    remove matdatasource and modified groupping code

--- src/app/table-group/table-group.component.ts
+++ src/app/table-group/table-group.component.ts
@@ -1,58 +1,29 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { MatTableDataSource } from "@angular/material/table";
-
-export class Group {
-  level = 0;
-  parent: Group;
-  expanded = true;
-  totalCounts = 0;
-  get visible(): boolean {
-    return !this.parent || (this.parent.visible && this.parent.expanded);
-  }
-}
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-table-group",
   templateUrl: "./table-group.component.html",
   styleUrls: ["./table-group.component.scss"]
 })
-export class TableGroupComponent implements OnInit {
+export class TableGroupComponent {
   @Input() columns: string[] = [];
-  @Input() data$;
   @Input() indentationSize: number = 20;
   @Input() allExpanded: boolean = true;
+  @Input() groupByColumns: string[] = [];
 
-  _alldata: any[];
-  groupByColumns: string[] = [];
+  private _alldata: any[];
+  private _grouppedData: any[];
+  public dataSource = [];
+
+  @Input()
+  set data(value: any[]) {
+    if (!value) return;
+    this._alldata = value;
+    this.updateGroup();
+  }
 
   get displayedColumns(): string[] {
-    return [
-      "indentation",
-      ...this.columns.filter(c => !this.groupByColumns.includes(c))
-    ];
-  }
-
-  public dataSource = new MatTableDataSource<any | Group>([]);
-
-  constructor() {}
-
-  ngOnInit() {
-    this.data$.subscribe(
-      (data: any) => {
-        this._alldata = data;
-        this.dataSource.data = this.addGroups(
-          this._alldata,
-          this.groupByColumns
-        );
-        this.dataSource.filterPredicate = this.customFilterPredicate.bind(this);
-        this.dataSource.filter = performance.now().toString();
-      },
-      (err: any) => console.log(err)
-    );
-  }
-
-  getIndentation() {
-    return this.indentationSize * this.groupByColumns.length + 1 + "px";
+    return this.columns.filter(c => !this.groupByColumns.includes(c));
   }
 
   drop(event) {
@@ -69,122 +40,95 @@ export class TableGroupComponent implements OnInit {
   }
 
   groupBy(column) {
-    this.checkGroupByColumn(column, true);
-    this.dataSource.data = this.addGroups(this._alldata, this.groupByColumns);
-    this.dataSource.filter = performance.now().toString();
-  }
-
-  checkGroupByColumn(field, add) {
-    let found = null;
-    for (const column of this.groupByColumns) {
-      if (column === field) {
-        found = this.groupByColumns.indexOf(column, 0);
-      }
-    }
-    if (found != null && found >= 0) {
-      if (!add) {
-        this.groupByColumns.splice(found, 1);
-      }
-    } else {
-      if (add) {
-        this.groupByColumns.push(field);
-      }
-    }
+    this.groupByColumns.push(column);
+    this.updateGroup();
   }
 
   unGroupBy(column) {
-    this.checkGroupByColumn(column, false);
-    this.dataSource.data = this.addGroups(this._alldata, this.groupByColumns);
-    this.dataSource.filter = performance.now().toString();
+    this.groupByColumns.splice(this.groupByColumns.indexOf(column), 1);
+    this.updateGroup();
   }
 
-  // below is for grid row grouping
-  customFilterPredicate(data: any | Group, filter: string): boolean {
-    return data instanceof Group ? data.visible : this.getDataRowVisible(data);
-  }
-
-  getDataRowVisible(data: any): boolean {
-    const groupRows = this.dataSource.data.filter(row => {
-      if (!(row instanceof Group)) {
-        return false;
-      }
-      let match = true;
-      this.groupByColumns.forEach(column => {
-        if (!row[column] || !data[column] || row[column] !== data[column]) {
-          match = false;
-        }
-      });
-      return match;
-    });
-
-    if (groupRows.length === 0) {
-      return true;
-    }
-    const parent = groupRows[0] as Group;
-    return parent.visible && parent.expanded;
-  }
-
-  groupHeaderClick(row) {
-    row.expanded = !row.expanded;
-    this.dataSource.filter = performance.now().toString(); // bug here need to fix
-  }
-
-  addGroups(data: any[], groupByColumns: string[]): any[] {
-    const rootGroup = new Group();
-    rootGroup.expanded = true;
-    return this.getSublevel(data, 0, groupByColumns, rootGroup);
-  }
-
-  getSublevel(
-    data: any[],
-    level: number,
-    groupByColumns: string[],
-    parent: Group
-  ): any[] {
-    if (level >= groupByColumns.length) {
-      return data;
-    }
-    const groups = this.uniqueBy(
-      data.map(row => {
-        const result = new Group();
-        result.level = level + 1;
-        result.parent = parent;
-        for (let i = 0; i <= level; i++) {
-          result[groupByColumns[i]] = row[groupByColumns[i]];
-        }
-        return result;
-      }),
-      JSON.stringify
+  updateGroup() {
+    this._grouppedData = convertToGrouppedData(
+      this.groupByColumns,
+      this._alldata
     );
-
-    const currentColumn = groupByColumns[level];
-    let subGroups = [];
-    groups.forEach(group => {
-      const rowsInGroup = data.filter(
-        row => group[currentColumn] === row[currentColumn]
-      );
-      group.totalCounts = rowsInGroup.length;
-      const subGroup = this.getSublevel(
-        rowsInGroup,
-        level + 1,
-        groupByColumns,
-        group
-      );
-      subGroup.unshift(group);
-      subGroups = subGroups.concat(subGroup);
-    });
-    return subGroups;
+    this.dataSource = this._grouppedData.filter(d => d.$$visible);
   }
 
-  uniqueBy(a, key) {
-    const seen = {};
-    return a.filter(item => {
-      const k = key(item);
-      return seen.hasOwnProperty(k) ? false : (seen[k] = true);
-    });
+  groupHeaderClick(group) {
+    group.$$expanded = !group.$$expanded;
+    let index = this._grouppedData.indexOf(group);
+    let row = this._grouppedData[++index];
+    const expansionTracking = [];
+    expansionTracking[group.$$level] = group.$$expanded;
+    while (row && row.$$level > group.$$level) {
+      row.$$visible = group.$$expanded
+        ? expansionTracking[row.$$level - 1]
+        : false;
+      expansionTracking[row.$$level] = !!row.$$expanded && !!row.$$visible;
+      row = this._grouppedData[++index];
+    }
+    this.dataSource = this._grouppedData.filter(d => d.$$visible);
   }
 
   isGroup(index, item): boolean {
-    return item.level;
+    return item.$$isGroup;
   }
 }
+
+const convertToGrouppedData = (
+  groupBy: string[] = [],
+  tableData: any[],
+  allExpanded = true
+): any[] => {
+  if (!groupBy.length) return tableData;
+
+  const groups = {};
+
+  for (const data of tableData) {
+    let tempGroup: any = groups;
+    for (const group of groupBy) {
+      tempGroup[data[group]] = tempGroup[data[group]] || {};
+      tempGroup = tempGroup[data[group]];
+    }
+    tempGroup._values = tempGroup._values || [];
+    tempGroup._count = tempGroup._count || 0;
+    tempGroup._count++;
+    tempGroup._values.push(data);
+  }
+
+  const grouppedData = [];
+  const recursion = (group, level = 0) => {
+    if (group._values) {
+      grouppedData.push(
+        ...group._values.map(v => {
+          v.$$visible = allExpanded;
+          v.$$level = level;
+          return v;
+        })
+      );
+      return group._count;
+    }
+
+    let count = 0;
+    for (const prop in group) {
+      const groupRow: any = {};
+      grouppedData.push(groupRow);
+      groupRow[groupBy[level]] = prop;
+      groupRow.$$isGroup = true;
+      groupRow.$$level = level;
+      groupRow.$$expanded = allExpanded;
+      groupRow.$$visible = allExpanded || level === 0;
+      groupRow.$$count = recursion(group[prop], level + 1);
+      count += groupRow.$$count;
+    }
+
+    return count;
+  };
+
+  recursion(groups);
+
+  return grouppedData;
+};
